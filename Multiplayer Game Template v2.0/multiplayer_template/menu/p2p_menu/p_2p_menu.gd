b074ff7d75cfc6340bff18370c206726cc2604c4
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2025-01-04 22:47:55 -0600

    Began menu

---
+++ Multiplayer Game Template v2.0/multiplayer_template/menu/p2p_menu/p_2p_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@export var sub_menu_holder : SubMenuHolder
+
+
+func _on_host_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_join_pressed() -> void:
+	pass # Replace with function body.
